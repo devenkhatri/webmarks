86c9cfbdfc1088cb47520c57e822524d2338f793
devenkhatri <deven.khatri@gmail.com>
2022-04-21 10:50:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,10 +28,10 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/" exact={true}>
+        {/* <Route path="/" exact={true}>
           <Redirect to="/home" />
-        </Route>
-        <Route path="/home" exact={true}>
+        </Route> */}
+        <Route path="/" exact={true}>
           <Home />
         </Route>
       </IonRouterOutlet>
